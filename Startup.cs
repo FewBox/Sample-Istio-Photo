2a3b5960af7daa2cb43e87c7383c25b92b42f926
landpy <landpy@outlook.com>
2019-01-01 14:09:32 +0800

    Add CORS to service.

--- Startup.cs
+++ Startup.cs
@@ -25,6 +25,7 @@ namespace Sample_Istio_Photo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
