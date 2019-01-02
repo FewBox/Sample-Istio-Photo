a33598555f3d6ef3a52637572db6c1c44a64ae1e
landpy <landpy@outlook.com>
2019-01-02 23:15:34 +0800

    Change unsplash urls.

--- Dtos/UrlsDto.cs
+++ Dtos/UrlsDto.cs
@@ -3,5 +3,9 @@ namespace Sample_Istio_Photo.Dtos
     public class UrlsDto
     {
         public string Raw {get;set;}
+        public string Full { get; set; }
+        public string Regular { get; set; }
+        public string Small { get; set; }
+        public string Thumb { get; set; }
     }
 }
--- Repositories/Urls.cs
+++ Repositories/Urls.cs
@@ -5,5 +5,9 @@ namespace Sample_Istio_Photo.Repositories
     public class Urls
     {
         public string Raw { get; set; }
+        public string Full { get; set; }
+        public string Regular { get; set; }
+        public string Small { get; set; }
+        public string Thumb { get; set; }
     }
 }
