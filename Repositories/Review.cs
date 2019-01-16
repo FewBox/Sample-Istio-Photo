bd4fc88c1eb02fe7eda2de704a20f7ec113756a8
Peter Pang <mv@live.cn>
2019-01-16 13:38:11 +0800

    Change star.

--- Repositories/Review.cs
+++ Repositories/Review.cs
@@ -5,6 +5,7 @@ namespace Sample_Istio_Photo.Repositories
     public class Review
     {
         public string Content{get;set;}
-        public string Base64SvgStar { get; set; }
+        public string Base64SvgAvatar { get; set; }
+        public int Star { get; set; }
     }
 }
