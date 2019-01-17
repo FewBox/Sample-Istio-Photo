e426abaa8ee208f1ae154ca0659f7d00e2ecd3a6
Peter Pang <mv@live.cn>
2019-01-17 15:57:04 +0800

    Fix bug.

--- Repositories/Review.cs
+++ Repositories/Review.cs
@@ -5,6 +5,6 @@ namespace Sample_Istio_Photo.Repositories
     public class Review
     {
         public string Content{get;set;}
-        public string Base64SvgStar { get; set; }
+        public string Base64SvgAvatar { get; set; }
     }
 }
